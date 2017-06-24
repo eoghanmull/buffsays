99138f08fa142c9bdb6f4272db2041a32c088848
Ciaran <ciaran.omara@linkiotech.com>
2017-06-24 17:04:29 +0100

    slash icon and clean up

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -12,12 +12,7 @@ export class InfoModal {
     constructor(private nav:NavController, private viewCtrl:ViewController) {
 
     }
-    public contributors = [
-            'Eoghan Mulcahy', 
-            'E-mail - eoghanm56@gmail.com',
-            'Ciarán O\'Mara',
-            'E-mail - ciaranahane@gmail.com'
-        ];
+
 
     dismiss(){
       this.viewCtrl.dismiss({});
